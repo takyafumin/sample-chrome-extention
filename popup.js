136a8452bf75116260761b352ad94e3a3e495991
takyafumin <takyafumin@gmail.com>
2023-11-18 15:55:57 +0900

    add set keyword for google

--- popup.js
+++ popup.js
@@ -7,6 +7,21 @@ document.getElementById("btn").addEventListener("click", async () => {
   });
 });
 
+document.getElementById("btn-set-google-search-keyword").addEventListener("click", async () => {
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setGoogleSearchKeyword,
+  });
+});
+
 function onRun() {
   document.body.style.backgroundColor = "#fcc"
 }
+
+const setGoogleSearchKeyword = () => {
+  const input = document.getElementById('APjFqb');
+  input.value = 'Search Keyword Set!!';
+}
+
